75a726b7e9e77d5746b34f1736d7008aa6046817
MetehanSevgil <030122059@std.izu.edu.tr>
2024-08-28 09:45:02 +0300

    node.js kaldırıldı

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        alert('Login Successful!');
+        console.log('JWT Token:', data.token);
+    } else {
+        alert('Login Failed!');
+    }
+});
+
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('regUsername').value;
+    const password = document.getElementById('regPassword').value;
+
+    const response = await fetch('http://localhost:5500/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+        alert('Registration Successful!');
+    } else {
+        alert('Registration Failed!');
+    }
+});
